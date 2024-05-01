4ffdc7661d40d8dd16611f6adda94a12c52e2983
Jhonata Costa <costajhonata8@gmail.com>
2024-05-01 15:14:25 -0300

    Kafka cofigs

---
+++ str-producer/src/main/java/com/ms/strproducer/controller/StringProducerController.java
@@ -0,0 +1,23 @@
+package com.ms.strproducer.controller;
+
+import com.ms.strproducer.service.StringProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/producer")
+public class StringProducerController {
+
+    @Autowired
+    private StringProducerService producerService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(String message){
+        producerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
